9569bafbce7a8e3431b2cd8fa690c19b263585da
Zahary <zaharymomchilov@gmail.com>
2019-03-15 20:17:07 +0200

    Added coord info

--- RFID/rfid_esp8266.ino
+++ RFID/rfid_esp8266.ino
@@ -1,44 +1,55 @@
+/*/
+
+   RST/Reset   RST          D1 (GPIO5)        
+ * SPI SS      SDA(SS)      D2 (GPIO4)       
+ * SPI MOSI    MOSI         D7 (GPIO13)
+ * SPI MISO    MISO         D6 (GPIO12)
+ * SPI SCK     SCK          D5 (GPIO14)
+
+/*/
+
+
 #include <SPI.h>
 #include <MFRC522.h>
- 
+
 constexpr uint8_t RST_PIN = 5;     // Configurable, see typical pin layout above
 constexpr uint8_t SS_PIN = 4;     // Configurable, see typical pin layout above
  
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
- 
+
 MFRC522::MIFARE_Key key; 
- 
+
 // Init array that will store new NUID 
 byte nuidPICC[4];
- 
+
 void setup() { 
   Serial.begin(115200);
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
- 
+
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
- 
+
   Serial.println(F("This code scan the MIFARE Classsic NUID."));
   Serial.print(F("Using the following key:"));
   printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
 }
  
 void loop() {
- 
+
   // Look for new cards
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
- 
+
   // Verify if the NUID has been readed
   if ( ! rfid.PICC_ReadCardSerial())
     return;
- 
+
   Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
   Serial.println(rfid.PICC_GetTypeName(piccType));
- 
+
   // Check is the PICC of Classic MIFARE type
   if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
     piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
@@ -46,13 +57,13 @@ void loop() {
     Serial.println(F("Your tag is not of type MIFARE Classic."));
     return;
   }
- 
+
   if (rfid.uid.uidByte[0] != nuidPICC[0] || 
     rfid.uid.uidByte[1] != nuidPICC[1] || 
     rfid.uid.uidByte[2] != nuidPICC[2] || 
     rfid.uid.uidByte[3] != nuidPICC[3] ) {
     Serial.println(F("A new card has been detected."));
- 
+
     // Store NUID into nuidPICC array
     for (byte i = 0; i < 4; i++) {
       nuidPICC[i] = rfid.uid.uidByte[i];
@@ -67,15 +78,15 @@ void loop() {
     Serial.println();
   }
   else Serial.println(F("Card read previously."));
- 
+
   // Halt PICC
   rfid.PICC_HaltA();
- 
+
   // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
 }
- 
- 
+
+
 /**
  * Helper routine to dump a byte array as hex values to Serial. 
  */
@@ -85,7 +96,7 @@ void printHex(byte *buffer, byte bufferSize) {
     Serial.print(buffer[i], HEX);
   }
 }
- 
+
 /**
  * Helper routine to dump a byte array as dec values to Serial.
  */
