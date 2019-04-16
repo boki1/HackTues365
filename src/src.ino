01b9effaad7afe988e6c5b6382840ca8c1432c39
Zahary <zaharymomchilov@gmail.com>
2019-04-16 18:58:20 +0300

    edit imges WORKING

--- src/src.ino
+++ src/src.ino
@@ -26,8 +26,8 @@ AsyncUDP udp;
 #define COLUMN_COUNT 3
 
 #ifndef STASSID
-#define STASSID "Robotics"
-#define STAPSK "elsysbot&"
+#define STASSID "Gandalf"
+#define STAPSK "wnxg3298"
 #endif
 
 const char *ssid = STASSID;
