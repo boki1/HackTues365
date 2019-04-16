30b76e0785ff74811b888de28481a87628704350
Zahary <zaharymomchilov@gmail.com>
2019-04-16 20:58:19 +0300

    Working EDIT UI function

--- SERVOS/Sweep/Sweep.ino
+++ SERVOS/Sweep/Sweep.ino
@@ -22,38 +22,38 @@ void loop() {
   for (pos1 = 0; pos1 <= 90; pos1 += 1) { 
     // in steps of 1 degree
     servo1.write(pos1);              
-    delay(10);                       
+    delay(13);                       
   }
   for (pos1 = 90; pos1 >= 0; pos1 -= 1) { 
     servo1.write(pos1);              
-    delay(10);                    
+    delay(13);                    
   }
   for (pos2 = 0; pos2 <= 70; pos2 += 1) { 
     // in steps of 1 degree
     servo2.write(pos2);              
-    delay(10);                     
+    delay(13);                     
   }
   for (pos2 = 70; pos2 >= 0; pos2 -= 1) { 
     servo2.write(pos2);              
-    delay(10);                       
+    delay(13);                       
   }
   for (pos3 = 0; pos3 <= 90; pos3 += 1) { 
     // in steps of 1 degree
     servo3.write(pos3);              
-    delay(10);                       
+    delay(13);                       
   }
   for (pos3 = 90; pos3 >= 0; pos3 -= 1) { 
     servo3.write(pos3);              
-    delay(10
+    delay(13
     );                       
   }
   for (pos4 = 0; pos4 <= 70; pos4 += 1) { 
     // in steps of 1 degree
     servo4.write(pos4);              
-    delay(10);                       
+    delay(13);                       
   }
   for (pos4 = 70; pos4 >= 0; pos4 -= 1) { 
     servo4.write(pos4);              
-    delay(10);                       
+    delay(13);                       
   }
 }
